0479cabf29cfc486bd324796d8df8d8541473aaf
Joseph Matthew Caballas <joseph@mahusai.ph>
2022-06-07 01:22:04 +0800

    Create Product model

--- shoppingcart/models.py
+++ shoppingcart/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import uuid  # for unique SKU
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    SKU = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique SKU for this particular product')
+    description = models.TextField(help_text='Brief description of the product')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
