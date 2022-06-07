21ffe002db28c695562e7cf5ab05709a089b8507
Joseph Matthew Caballas <joseph@mahusai.ph>
2022-06-07 11:58:20 +0800

    Fix installation typo

--- cartsite/settings.py
+++ cartsite/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shoppingcart.apps.ShoppingcartConfig'
-    'users.apps.UsersConfig'
+    'shoppingcart.apps.ShoppingcartConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
