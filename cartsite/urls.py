5fb9925ae737121da882924633ece763968a6e48
Joseph Matthew Caballas <joseph@mahusai.ph>
2022-06-13 13:08:39 +0800

    Create base.html with corresponding url and view

--- cartsite/urls.py
+++ cartsite/urls.py
@@ -18,8 +18,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from shoppingcart import views as shoppingcart_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', shoppingcart_views.shoppingcart, name='shoppingcart')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
