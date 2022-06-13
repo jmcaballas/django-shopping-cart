7bde0327c698206f5ae32dcacfbbcafaa7903bab
Joseph Matthew Caballas <joseph@mahusai.ph>
2022-06-13 14:55:10 +0800

    Require log-in for cart page

--- shoppingcart/views.py
+++ shoppingcart/views.py
@@ -2,9 +2,9 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
-# @login_required
 def home(request):
     return render(request, 'shoppingcart/home.html', {})
 
+@login_required
 def cart(request):
     return render(request, 'shoppingcart/cart.html', {})
