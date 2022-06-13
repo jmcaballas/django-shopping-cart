9162c6d3e3cb2474746eee796044f9bc38d9dea8
Joseph Matthew Caballas <joseph@mahusai.ph>
2022-06-13 22:00:31 +0800

    Make a separate urlpatterns for shoppingcart app

--- cartsite/urls.py
+++ cartsite/urls.py
@@ -17,15 +17,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, include
 
-from shoppingcart import views as shoppingcart_views
 from users import views as user_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', shoppingcart_views.home, name='home'),
-    path('cart/', shoppingcart_views.cart, name='cart'),
+    path('', include("shoppingcart.urls")),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', user_views.register, name='register'),
