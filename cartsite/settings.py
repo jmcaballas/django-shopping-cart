20935f3efd96d0adee98362e9c2649b88361638f
Joseph Matthew Caballas <joseph@mahusai.ph>
2022-06-13 14:18:17 +0800

    Add logout redirect

--- cartsite/settings.py
+++ cartsite/settings.py
@@ -135,3 +135,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.Account'
 
 LOGIN_REDIRECT_URL = 'shoppingcart'
+LOGIN_URL = 'login'
+LOGOUT_REDIRECT_URL = 'shoppingcart'
--- cartsite/urls.py
+++ cartsite/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', shoppingcart_views.shoppingcart, name='shoppingcart'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', user_views.register, name='register'),
 ]
 
