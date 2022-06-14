29201b10ac0e6f679515c6fe92245465fe5e2806
Joseph Matthew Caballas <joseph@mahusai.ph>
2022-06-14 15:08:59 +0800

    Add functionality to add to cart in home page

--- shoppingcart/urls.py
+++ shoppingcart/urls.py
@@ -7,6 +7,7 @@ app_name = "shoppingcart"
 
 urlpatterns = [
     path('', views.ProductListView.as_view(), name='home'),
+    path('cart/add/<slug:slug>', views.add_to_cart, name='add-to-cart'),
     path('cart/', views.cart, name='cart'),
     path('product/<slug:slug>', views.ProductDetailView.as_view(), name='details'),
 ]
--- shoppingcart/views.py
+++ shoppingcart/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
 
-from .models import Product
+from .models import Product, SaleOrder, SaleOrderItem
 
 
 class ProductListView(ListView):
@@ -16,6 +17,22 @@ class ProductListView(ListView):
 class ProductDetailView(DetailView):
     queryset = Product.objects.with_stock()
     template_name = "shoppingcart/details.html"
+
+
+@login_required
+def add_to_cart(request, slug):
+    cart_product = Product.objects.get(slug=slug)
+    cart_product_stock = Product.objects.with_stock().get(slug=slug).stock
+    quantity = 1
+
+    sale_order = SaleOrder.objects.get_or_create(user=request.user)
+    sale_order_item = SaleOrderItem.objects.create(
+        product=cart_product,
+        quantity=quantity,
+        sale_order=sale_order[0],
+        unit_price=quantity*cart_product.price
+    )
+    return HttpResponse("Item added to cart", request)
 
 
 @login_required
